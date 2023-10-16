42788070bc92f23dcae0e45c5612f401ae28e0cc
maddiesolis <madelinesdeo@gmail.com>
2023-10-15 22:52:32 -0700

    basic background and color functionality

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,21 +4,33 @@ import doorway from "../images/doorway.png"
 import mirror from "../images/mirror.png"
 import touch from "../images/touch.png"
 import { Title } from "./Title";
+import { doorwayBackgroundColors, images, mirrorBackgroundColors, touchBackgroundColors } from "../colors";
 
 export interface StyledComponentProps {
   color: string;
 }
 
+const TitleContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 1rem;
+`
+
+const TitleSpan = styled.span<StyledComponentProps>`
+  font-size: 50px;
+  color: ${props => props.color || 'black'};
+`;
+
 const PageContainerDiv = styled.div<StyledComponentProps>`
   display: grid;
   grid-template-columns: 60% 40%;
   height: 100vh;
-  background-color: ${props => props.color || 'white'};;
+  background-color: ${props => props.color};;
 `;
 
 const LhsContainerDiv = styled.div`
   display: grid;
-  grid-template-rows: 20% 80%;
+  grid-template-rows: 40% 60%;
   border: solid black 2px;
 `;
 
@@ -39,12 +51,10 @@ const ArtPieceDiv = styled.div`
 `;
 
 const PoemSpan = styled.span`
-  background-color: lightcoral;
   border: solid black 2px;
 `;
 
 const AnimationDiv = styled.span`
-  background-color: lightgreen;
   border: solid black 2px;
 `
 
@@ -53,50 +63,49 @@ export type ImageColorPairs = {
 }
 
 export const PageLayout = () => {
-  const [randomImage, setRandomImage] = useState({
-    name: "doorway",
+  const [image, setImage] = useState({
+    name: 'doorway',
     src: doorway
   });
-  const [randomBackground, setRandomBackground] = useState("white");
-  
-  const images = useMemo(() => [
-    {
-      name: "doorway",
-      src: doorway
-    },
-    {
-      name: "mirror",
-      src: mirror
-    },
-    {
-      name: "touch",
-      src: touch
-    }
-  ], []);
-
-  const backgrounds: ImageColorPairs = useMemo(() => ({
-    doorway: ["#C66D40", "#705E5E", "#4E454F", "#664442", "#232939", "#4B222C"],
-    mirror: ["#828894", "#6B6B77", "#CE9781", "#616161", "#3F3F3F", "#727272"],
-    touch: ["#787774", "#313332", "#B2B3B3", "#D6A369", "#554631", "#987062"]
-  }), []);
+  const [background, setBackground] = useState("#717171");
+  const [combo, setCombo] = useState(
+    {primary: 'black', secondary: 'black', tertiary: 'black', quaternary: 'black'},
+  )
 
   useEffect(() => {
-    const randomImageIndex = Math.floor(Math.random() * images.length);
-    const newRandomImage = images[randomImageIndex];
-    setRandomImage(newRandomImage);
+    const imageIndex = Math.floor(Math.random() * images.length)
+    const selectedImage = images[imageIndex]
+    setImage(selectedImage)  // might need to make this a variable first then pass it in
 
-    const backgroundForImage = backgrounds[newRandomImage.name];
-    const randomBackgroundIndex = Math.floor(Math.random() * backgroundForImage.length);
-    const newRandomBackground = backgroundForImage[randomBackgroundIndex];
-    setRandomBackground(newRandomBackground);
-  }, [images, backgrounds]);
+    let backgrounds: typeof doorwayBackgroundColors | typeof mirrorBackgroundColors | typeof touchBackgroundColors = [];
+    if (selectedImage.name == 'doorway') {
+      backgrounds = doorwayBackgroundColors
+    } else if (selectedImage.name == 'mirror') {
+      backgrounds = mirrorBackgroundColors
+    } else if (selectedImage.name == 'touch') {
+      backgrounds = touchBackgroundColors
+    }
+    const backgroundIndex = Math.floor(Math.random() * backgrounds.length)
+    const selectedBackground = backgrounds[backgroundIndex].code
+    setBackground(selectedBackground)
+
+    const combos = backgrounds[backgroundIndex].combos
+    const comboIndex = Math.floor(Math.random() * combos.length)
+    const selectedCombo = combos[comboIndex]
+    setCombo(selectedCombo)
+  }, []);
 
   return (
-    <PageContainerDiv color={randomBackground}>
+    <PageContainerDiv color={background}>
       <LhsContainerDiv>
-        <Title imageName={randomImage.name} background={randomBackground}/>
+      <TitleContainer>
+            <TitleSpan color={combo.primary}>Primary</TitleSpan>
+            <TitleSpan color={combo.secondary}>Secondary</TitleSpan>
+            <TitleSpan color={combo.tertiary}>Tertiary</TitleSpan>
+            <TitleSpan color={combo.quaternary}>Quaternary</TitleSpan>
+        </TitleContainer>
         <ArtPieceDiv>
-          <img src={randomImage.src} alt={"Random"}></img>
+          <img src={image.src} alt={"Random"}></img>
         </ArtPieceDiv>
       </LhsContainerDiv>
       <RhsContainerDiv>
